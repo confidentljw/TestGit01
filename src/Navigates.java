d7aee788e25b3ff53d5d85a40654545cd6c72424
mv <confident_ljwsts@163.com>
2017-05-10 09:58:14 +0800

    (Analyze)

--- src/Navigates.java
+++ src/Navigates.java
@@ -11,5 +11,6 @@ public class Navigates {
     private Navigates() {
         System.out.println("Navigate 导航");
         System.out.println("Code 代码");
+        System.out.println("Analyze 分析");
     }
 }
