d05ca65302039981da1d031cdaea23ec7e74ca52
mv <confident_ljwsts@163.com>
2017-05-10 09:48:41 +0800

    append Navigates java

---
+++ src/Navigates.java
@@ -0,0 +1,15 @@
+/**
+ * Created by Administrator on 2017/5/10.
+ */
+public class Navigates {
+    private static Navigates ourInstance = new Navigates();
+
+    public static Navigates getInstance() {
+        return ourInstance;
+    }
+
+    private Navigates() {
+
+
+    }
+}
